ad30fdfd6317b1871488c325564fd34271bdaab0
Patryk <patrykwyczesany75@gmail.com>
2020-11-24 14:33:51 +0100

    init

---
+++ app/src/main/java/org/altbeacon/beaconreference/BackgroundTask.java
@@ -0,0 +1,30 @@
+package org.altbeacon.beaconreference;
+
+import android.os.AsyncTask;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class BackgroundTask extends AsyncTask<String,Void,Void> {
+    private Socket s;
+    private PrintWriter writer;
+
+    @Override
+    protected Void doInBackground(String... voids) {
+        try
+        {
+            String message=voids[0];
+            s=new Socket("192.168.1.48",6000);
+            System.out.println(message+"HEHEGIVE");
+            writer= new PrintWriter(s.getOutputStream());
+            writer.write(message);
+            writer.flush();
+            writer.close();
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
---
+++ app/src/main/java/org/altbeacon/beaconreference/RangingActivity.java
@@ -0,0 +1,100 @@
+package org.altbeacon.beaconreference;
+
+import java.util.Collection;
+
+import android.app.Activity;
+
+import android.os.Bundle;
+import android.os.RemoteException;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.altbeacon.beacon.AltBeacon;
+import org.altbeacon.beacon.Beacon;
+import org.altbeacon.beacon.BeaconConsumer;
+import org.altbeacon.beacon.BeaconManager;
+import org.altbeacon.beacon.BeaconParser;
+import org.altbeacon.beacon.RangeNotifier;
+import org.altbeacon.beacon.Region;
+
+public class RangingActivity extends Activity implements BeaconConsumer{
+    protected static final String TAG = "RangingActivity";
+    static String beaconId;
+    static String beaconIncomingId;
+    private BeaconManager beaconManager = BeaconManager.getInstanceForApplication(this);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        beaconId="0";
+        beaconIncomingId =getIntent().getStringExtra("beaconID");
+        setContentView(R.layout.activity_ranging);
+    }
+
+    @Override 
+    protected void onDestroy() {
+        beaconId="0";
+        super.onDestroy();
+    }
+
+    @Override 
+    protected void onPause() {
+        super.onPause();
+        beaconId="0";
+        beaconManager.unbind(this);
+    }
+
+    @Override 
+    protected void onResume() {
+        beaconId="0";
+        super.onResume();
+        beaconManager.bind(this);
+    }
+public void onAttendanceClicked(View view){
+    TextView viewNameSurname;
+    EditText editNameSurname;
+    String putNameSurname;
+    System.out.println(beaconId+"HERE"+beaconIncomingId);
+    editNameSurname  = (EditText) findViewById(R.id.editText2);
+    viewNameSurname=(TextView) findViewById(R.id.textView2);
+    putNameSurname=editNameSurname.getText().toString();
+        if(beaconId.equals(beaconIncomingId)){
+            viewNameSurname.setText(putNameSurname);
+        }
+        else
+            viewNameSurname.setText("You are not");
+}
+    @Override
+    public void onBeaconServiceConnect() {
+
+        RangeNotifier rangeNotifier = new RangeNotifier() {
+           @Override
+           public void didRangeBeaconsInRegion(Collection<Beacon> beacons, Region region) {
+              if (beacons.size() > 0) {
+                  Log.d(TAG, "didRangeBeaconsInRegion called with beacon count:  "+beacons.size());
+                  Beacon firstBeacon = beacons.iterator().next();
+                  logToDisplay("The beacon id: " + firstBeacon.getId1().toString() + " is about " + firstBeacon.getDistance() + " meters away.");
+                  beaconId=firstBeacon.getId1().toString();
+              }
+           }
+
+        };
+        try {
+            beaconManager.startRangingBeaconsInRegion(new Region("myRangingUniqueId", null, null, null));
+            beaconManager.addRangeNotifier(rangeNotifier);
+            beaconManager.startRangingBeaconsInRegion(new Region("myRangingUniqueId", null, null, null));
+            beaconManager.addRangeNotifier(rangeNotifier);
+        } catch (RemoteException e) {   }
+    }
+
+    private void logToDisplay(final String line) {
+        runOnUiThread(new Runnable() {
+            public void run() {
+                EditText editText = (EditText)RangingActivity.this.findViewById(R.id.rangingText);
+                editText.setText(line);
+            }
+        });
+    }
+}
