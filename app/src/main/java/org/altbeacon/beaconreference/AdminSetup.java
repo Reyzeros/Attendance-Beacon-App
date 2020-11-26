601625b557b2e67dc404f3f45edd0ca17fa3588a
Patryk <patrykwyczesany75@gmail.com>
2020-11-26 15:59:52 +0100

    AdminSetup menu adjusted and onClick set up

--- app/src/main/java/org/altbeacon/beaconreference/AdminSetup.java
+++ app/src/main/java/org/altbeacon/beaconreference/AdminSetup.java
@@ -2,8 +2,11 @@ package org.altbeacon.beaconreference;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
@@ -16,11 +19,24 @@ public class AdminSetup extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_setup);
 
-    //    editTextOrganiserName=(EditText) findViewById(R.id.editTextOrganiserName);
-     //   editTextBeaconID=(EditText) findViewById(R.id.editTextOrganiserBeaconId);
-     //   editTextGroupName=(EditText) findViewById(R.id.editTextGroupName);
-     //   buttonAddOrganiser=(Button) findViewById(R.id.buttonAddOrganiser);
-     //   buttonAddGroup=(Button) findViewById(R.id.buttonAddGroup);
+        editTextOrganiserName=(EditText) findViewById(R.id.editTextOrganiserName);
+        editTextBeaconID=(EditText) findViewById(R.id.editTextOrganiserBeaconId);
+        editTextGroupName=(EditText) findViewById(R.id.editTextGroupName);
+        buttonAddOrganiser=(Button) findViewById(R.id.buttonAddOrganiser);
+        buttonAddGroup=(Button) findViewById(R.id.buttonAddGroup);
+    }
+    public void onAddOrganiserClicked(View view){
+    addOrganiser();
+    }
+
+    private void addOrganiser(){
+        String organiserName=editTextOrganiserName.getText().toString().trim();
+        String beaconId=editTextBeaconID.getText().toString().trim();
+        if(!TextUtils.isEmpty(organiserName)&&!TextUtils.isEmpty(beaconId)){
+        }
+        else{
+            Toast.makeText(this,"Name and Id cannot be empty!", Toast.LENGTH_LONG).show();
+        }
     }
 
     @Override
